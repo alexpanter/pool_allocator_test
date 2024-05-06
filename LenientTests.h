cb61deeb914add8d8713dfbebdb5ed57021111c8
Alexander <alex_c007@hotmail.com>
2024-05-06 15:43:33 +0200

    Lenient testing

--- LenientTests.h
+++ LenientTests.h
@@ -19,3 +19,154 @@ inline void lenient_test_alloc()
     assert(t != nullptr);
     assert(t->index == 0);
 }
+
+inline void lenient_test_alloc_too_much()
+{
+    LenientPoolAllocator<LenientTestStruct, 1> pa;
+    LenientTestStruct* t = pa.Alloc();
+    assert(t != nullptr);
+    assert(t->index == 0);
+
+    t = pa.Alloc();
+    assert(t == nullptr);
+}
+
+inline void lenient_test_alloc_free_alloc()
+{
+    LenientPoolAllocator<LenientTestStruct, 1> pa;
+    LenientTestStruct* t = pa.Alloc();
+    assert(t != nullptr);
+    assert(t->index == 0);
+    t->dummyValue = 43;
+
+    pa.Free(t);
+
+    t = pa.Alloc();
+    assert(t != nullptr);
+    assert(t->index == 0);
+    assert(t->dummyValue == 43);
+}
+
+template<uint16_t Size>
+inline void lenient_test_alloc_n()
+{
+    LenientPoolAllocator<LenientTestStruct, Size> pa;
+
+    for (uint16_t i = 0; i < Size; i++)
+    {
+        LenientTestStruct* t = pa.Alloc();
+        assert(t != nullptr);
+    }
+
+    LenientTestStruct* t = pa.Alloc();
+    assert(t == nullptr);
+}
+
+template<uint16_t Size>
+inline void lenient_test_alloc_free_alloc_n()
+{
+    LenientPoolAllocator<LenientTestStruct, Size> pa;
+    std::vector<LenientTestStruct*> v;
+    v.reserve(Size);
+
+    for (uint16_t i = 0; i < Size; i++)
+    {
+        LenientTestStruct* t = pa.Alloc();
+        assert(t != nullptr);
+        v.push_back(t);
+    }
+
+    {
+        LenientTestStruct* t = pa.Alloc();
+        assert(t == nullptr);
+    }
+
+    for (auto p : v)
+    {
+        pa.Free(p);
+    }
+
+    pa.Free(nullptr);
+    v.clear();
+
+    for (uint16_t i = 0; i < Size; i++)
+    {
+        LenientTestStruct* t = pa.Alloc();
+        assert(t != nullptr);
+    }
+
+    {
+        LenientTestStruct* t = pa.Alloc();
+        assert(t == nullptr);
+    }
+}
+
+template<uint16_t Size>
+inline void lenient_test_no_double_free()
+{
+    LenientPoolAllocator<LenientTestStruct, Size> pa;
+    std::vector<LenientTestStruct*> v;
+    v.reserve(Size);
+
+    for (uint16_t i = 0; i < Size; i++)
+    {
+        LenientTestStruct* t = pa.Alloc();
+        assert(t != nullptr);
+        v.push_back(t);
+    }
+
+    for (auto p : v)
+    {
+        pa.Free(p);
+    }
+    for (auto p : v)
+    {
+        pa.Free(p);
+    }
+
+    pa.Free(nullptr);
+    v.clear();
+
+    for (uint16_t i = 0; i < Size; i++)
+    {
+        LenientTestStruct* t = pa.Alloc();
+        assert(t != nullptr);
+    }
+}
+
+template<uint16_t Size>
+inline void lenient_test_random_order_free()
+{
+    LenientPoolAllocator<LenientTestStruct, Size> pa;
+    std::vector<LenientTestStruct*> v;
+    v.reserve(Size);
+
+    for (uint16_t i = 0; i < Size; i++)
+    {
+        LenientTestStruct* t = pa.Alloc();
+        assert(t != nullptr);
+        v.push_back(t);
+    }
+
+    std::random_device rd;
+    std::default_random_engine reng {rd()};
+    std::shuffle(v.begin(), v.end(), reng);
+
+    for (auto p : v)
+    {
+        pa.Free(p);
+    }
+    pa.Free(nullptr);
+    v.clear();
+
+    for (uint16_t i = 0; i < Size; i++)
+    {
+        LenientTestStruct* t = pa.Alloc();
+        assert(t != nullptr);
+    }
+
+    {
+        LenientTestStruct* t = pa.Alloc();
+        assert(t == nullptr);
+    }
+}
--- PoolAllocatorTest.cpp
+++ PoolAllocatorTest.cpp
@@ -26,11 +26,38 @@ void test_normal()
 void test_lenient()
 {
     lenient_test_alloc();
+    lenient_test_alloc_too_much();
+    lenient_test_alloc_free_alloc();
+
+    lenient_test_alloc_n<1>();
+    lenient_test_alloc_n<2>();
+    lenient_test_alloc_n<3>();
+    lenient_test_alloc_n<4>();
+    lenient_test_alloc_n<8>();
+    lenient_test_alloc_n<32>();
+    lenient_test_alloc_n<4096>();
+
+    lenient_test_alloc_free_alloc_n<1>();
+    lenient_test_alloc_free_alloc_n<2>();
+    lenient_test_alloc_free_alloc_n<3>();
+    lenient_test_alloc_free_alloc_n<4>();
+    lenient_test_alloc_free_alloc_n<128>();
+    lenient_test_alloc_free_alloc_n<4096>();
+
+    lenient_test_no_double_free<1>();
+    lenient_test_no_double_free<2>();
+    lenient_test_no_double_free<3>();
+    lenient_test_no_double_free<4>();
+
+    lenient_test_random_order_free<16>();
+    lenient_test_random_order_free<64>();
+    lenient_test_random_order_free<256>();
 }
 
 int main(int argc, char* argv[])
 {
-    test_normal();
+    //test_normal();
+    test_lenient();
 
     return 0;
 }
